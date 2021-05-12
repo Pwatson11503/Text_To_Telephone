fc3d41f50dcda9719e519710ddcbc8d192d5a05a
Preston Watson <prw20@txstate.edu>
2021-05-12 16:35:40 -0500

    Add project files.

---
+++ prw20_Chapter_8_9/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace prw20_Chapter_8_9
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnConvert = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.txtUserInput = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblOutput = new System.Windows.Forms.Label();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnConvert
+			// 
+			this.btnConvert.Location = new System.Drawing.Point(43, 115);
+			this.btnConvert.Name = "btnConvert";
+			this.btnConvert.Size = new System.Drawing.Size(75, 23);
+			this.btnConvert.TabIndex = 0;
+			this.btnConvert.Text = "Convert";
+			this.btnConvert.UseVisualStyleBackColor = true;
+			this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(85, 143);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 1;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// txtUserInput
+			// 
+			this.txtUserInput.Location = new System.Drawing.Point(67, 38);
+			this.txtUserInput.Name = "txtUserInput";
+			this.txtUserInput.Size = new System.Drawing.Size(111, 20);
+			this.txtUserInput.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(21, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(207, 26);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Please enter a 10 letter text in the format \r\nXXX-XXX-XXXX you would like convert" +
+    "ed.";
+			// 
+			// lblOutput
+			// 
+			this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblOutput.Location = new System.Drawing.Point(79, 73);
+			this.lblOutput.Name = "lblOutput";
+			this.lblOutput.Size = new System.Drawing.Size(85, 19);
+			this.lblOutput.TabIndex = 4;
+			this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(124, 115);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 5;
+			this.btnClear.Text = "Clear";
+			this.btnClear.UseVisualStyleBackColor = true;
+			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(249, 191);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.lblOutput);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtUserInput);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnConvert);
+			this.Name = "Form1";
+			this.Text = "Alphabet Tele Number";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnConvert;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.TextBox txtUserInput;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblOutput;
+		private System.Windows.Forms.Button btnClear;
+	}
+}
+
---
+++ prw20_Chapter_8_9/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prw20_Chapter_8_9
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnConvert_Click(object sender, EventArgs e)
+		{
+			ConvertText();
+		}
+
+		private void ConvertText()
+		{
+			//Do a ToUpper first to clean the text to compare against variables.
+			/*Set variables for VALID_LENGTH = 12
+			 *  •	A, B, and C = 2
+				•	D, E, and F = 3
+				•	G, H, and I = 4
+				•	J, K, and L = 5
+				•	M, N, and O = 6
+				•	P, Q, R, and S = 7
+				•	T, U, and V = 8
+				•	W, X, Y, and Z = 9*/
+			//Make sure formatting is correct (str.Length == VALID_LENGTH && str [3] == '-' && str[7] == '-'), if not error message
+			//if the user enters 555-GET-FOOD, the application should display 555-438-3663.
+			const int VALID_LENGTH = 12;
+			int abc = 2;
+			int def = 3;
+			int ghi = 4;
+			int jkl = 5;
+			int mno = 6;
+			int pqrs = 7;
+			int tuv = 8;
+			int wxyz = 9;
+			string str = "";
+			str = txtUserInput.Text.ToUpper();
+			string conversion = "";
+
+			if (str.Length == VALID_LENGTH && str[3] == '-' && str[7] == '-')
+			{
+				foreach (char ch in str)
+				{
+					if (ch == 'A' || ch ==  'B' || ch == 'C')
+					{
+						conversion += abc.ToString();
+					}
+					if (ch == 'D' || ch == 'E' || ch == 'F')
+					{
+						conversion += def.ToString();
+					}
+					if (ch == 'G' || ch == 'H' || ch == 'I')
+					{
+						conversion += ghi.ToString();
+					}
+					if (ch == 'J' || ch == 'K' || ch == 'L')
+					{
+						conversion += jkl.ToString();
+					}
+					if (ch == 'M' || ch == 'N' || ch == 'O')
+					{
+						conversion += mno.ToString();
+					}
+					if (ch == 'P' || ch == 'Q' || ch == 'R' || ch == 'S')
+					{
+						conversion += pqrs.ToString();
+					}
+					if (ch == 'T' || ch == 'U' || ch == 'V')
+					{
+						conversion += tuv.ToString();
+					}
+					if (ch == 'W' || ch == 'X' || ch == 'Y' || ch == 'Z')
+					{
+						conversion += wxyz.ToString();
+					}
+					if (ch == '0')
+					{ conversion += "0"; }
+					if (ch == '1')
+					{ conversion += "1"; }
+					if (ch == '2')
+					{ conversion += "2"; }
+					if (ch == '3')
+					{ conversion += "3"; }
+					if (ch == '4')
+					{ conversion += "4"; }
+					if (ch == '5')
+					{ conversion += "5"; }
+					if (ch == '6')
+					{ conversion += "6"; }
+					if (ch == '7')
+					{ conversion += "7"; }
+					if (ch == '8')
+					{ conversion += "8"; }
+					if (ch == '9')
+					{ conversion += "9"; }
+					if (conversion.Length == 3)
+					{
+						conversion += "-";
+					}
+					if (conversion.Length == 7)
+					{
+						conversion += "-";
+					}
+				}
+				lblOutput.Text = conversion;
+			}
+			else
+			{
+				MessageBox.Show("Please enter correct format");
+			}
+		}
+		private void ClearText()
+		{
+			txtUserInput.Text = "";
+			txtUserInput.Focus();
+			lblOutput.Text = "";
+		}
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			ClearText();
+		}
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
